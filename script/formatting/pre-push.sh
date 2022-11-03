fbb7dcd0bc4361a20bd8dccb8507a305162a8595
Kaushik Ravichandran <kaushikravichandran@lawn-128-61-36-70.lawn.gatech.edu>
2022-11-03 14:12:21 -0400

    add -u flag python

--- script/formatting/pre-push.sh
+++ script/formatting/pre-push.sh
@@ -5,7 +5,7 @@ NC='\033[0m'              # No Color
 
 echo "Linting code before commiting changes"
 echo "Running script/formatting/formatter.py"
-python script/formatting/formatter.py
+python -u script/formatting/formatter.py
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
     echo " ${Red}Oops! linting failed."
